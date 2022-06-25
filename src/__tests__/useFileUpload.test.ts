999ad346e5eef0d96b4fde84fbb5e92834e70f50
Dan <danielzuzevich@gmail.com>
2022-06-25 10:16:12 -0400

    added new tests

--- src/__tests__/useFileUpload.test.ts
+++ src/__tests__/useFileUpload.test.ts
@@ -1,17 +1,14 @@
+import { act } from 'react-test-renderer';
 import { renderHook } from '@testing-library/react-hooks';
 import { useFileUpload } from '../lib/useFileUpload';
 
-/**
- * @todo
- * - figure out an easy way to mock FileList
- */
 describe('useFileUpload', () => {
   it('Its initial values are correct', () => {
     const { result } = renderHook(() => useFileUpload());
     expect(result.current.files).toStrictEqual([]);
     expect(result.current.fileNames).toStrictEqual([]);
     expect(result.current.fileTypes).toStrictEqual([]);
-    expect(result.current.totalSize).toBe('');
+    expect(result.current.totalSize).toBe('0 Bytes');
     expect(result.current.totalSizeInBytes).toBe(0);
   });
 
@@ -28,5 +25,95 @@ describe('useFileUpload', () => {
     const { result } = renderHook(() => useFileUpload());
     const formData = result.current.createFormData();
     expect(formData instanceof FormData).toBe(true);
+  });
+
+  /**
+   * Adds new files via selection
+   */
+  it('adds new files via selection', () => {
+    const { result } = renderHook(() => useFileUpload());
+    const event = new Event('');
+
+    const file = new File(['foo'], 'foo.txt', {
+      type: 'text/plain',
+    });
+
+    Object.defineProperty(event, 'currentTarget', { writable: false, value: { files: [file] } });
+
+    act(() => {
+      // @ts-ignore
+      result.current.setFiles(event, 'a');
+    });
+
+    expect(result.current.files).toHaveLength(1);
+  });
+
+  /**
+   * Add files via drag and drop.
+   */
+  it('adds new files via drag and drop', () => {
+    const { result } = renderHook(() => useFileUpload());
+    const event = new InputEvent('');
+
+    const file = new File(['foo'], 'foo.txt', {
+      type: 'text/plain',
+    });
+
+    Object.defineProperty(event, 'dataTransfer', { writable: false, value: { files: [file] } });
+
+    act(() => {
+      // @ts-ignore
+      result.current.setFiles(event, 'a');
+    });
+
+    expect(result.current.files).toHaveLength(1);
+  });
+
+  /**
+   * Remove a specific file.
+   */
+  it('removes a specific file', () => {
+    const { result } = renderHook(() => useFileUpload());
+    const event = new Event('');
+
+    const file = new File(['foo'], 'foo.txt', {
+      type: 'text/plain',
+    });
+
+    Object.defineProperty(event, 'currentTarget', { writable: false, value: { files: [file] } });
+
+    act(() => {
+      // @ts-ignore
+      result.current.setFiles(event, 'a');
+    });
+
+    expect(result.current.files).toHaveLength(1);
+
+    act(() => result.current.removeFile('foo.txt'));
+    expect(result.current.files).toHaveLength(0);
+  });
+
+  /**
+   * Remove all files.
+   */
+  it('removes all files', () => {
+    const { result } = renderHook(() => useFileUpload());
+    const event = new Event('');
+
+    const file = new File(['foo'], 'foo.txt', {
+      type: 'text/plain',
+    });
+
+    Object.defineProperty(event, 'currentTarget', { writable: false, value: { files: [file, file, file] } });
+
+    act(() => {
+      // @ts-ignore
+      result.current.setFiles(event, 'a');
+    });
+
+    expect(result.current.files).toHaveLength(3);
+
+    act(() => result.current.removeFile('foo.txt'));
+    expect(result.current.files).toHaveLength(0);
   });
 });
